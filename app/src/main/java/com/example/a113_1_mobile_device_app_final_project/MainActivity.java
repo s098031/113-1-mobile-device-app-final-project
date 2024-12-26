335a872fd78daa1f703a3a2eb94efdd0a2b7560a
s098031 <tests098031@gmail.com>
2024-12-26 12:14:34 +0800

    first

---
+++ app/src/main/java/com/example/a113_1_mobile_device_app_final_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a113_1_mobile_device_app_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "113-1-mobile-device-app-final-project"
+include(":app")
